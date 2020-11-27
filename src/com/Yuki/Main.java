64a900b667b709c0c5f001956d4913244ec9b4b5
Tabanoon mim <74909482+tabanoonyuki8@users.noreply.github.com>
2020-11-27 11:35:02 +0600

    Add files via upload

---
+++ src/com/Yuki/Main.java
@@ -0,0 +1,49 @@
+package com.Yuki;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+                String name,adress,subject;
+                int roll;
+                float weather;
+
+                Scanner input=new Scanner(System.in);
+                System.out.println("Enter your Name:");
+                name=input.nextLine();
+
+                Scanner input2=new Scanner(System.in);
+                System.out.println("Enter your Adress:");
+                adress=input2.nextLine();
+
+                Scanner input3=new Scanner(System.in);
+                System.out.println("Enter your Subject:");
+                subject=input3.nextLine();
+
+                Scanner input4=new Scanner(System.in);
+                System.out.println("Enter your Roll:");
+                roll=input4.nextInt();
+
+                Scanner input5=new Scanner(System.in);
+                System.out.println("Today's weather:");
+                weather=input5.nextFloat();
+
+                LocalDate now=LocalDate.now();
+                //using java cureent time
+
+                System.out.println("Name:"+name);
+                System.out.println("Adress:"+adress);
+                System.out.println("Subject:"+subject);
+                System.out.println("Roll:"+roll);
+                System.out.println("\n");
+                System.out.println("Today's weather:"+weather+"F");
+                System.out.println("Today's date:"+now);
+
+
+
+    }
+}
